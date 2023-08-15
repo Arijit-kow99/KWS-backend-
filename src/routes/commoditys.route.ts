754f61bbf4864da6bd79cf79103aea5f04bd8745
Arijit <arijitkows@gmail.com>
2023-08-15 09:32:30 +0530

    commodity all services updated

--- src/routes/commoditys.route.ts
+++ src/routes/commoditys.route.ts
@@ -17,9 +17,9 @@ class CommoditysRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}`,  this.commoditysController.getCommodities);
     this.router.get(`${this.path}/:commodity_id`,  this.commoditysController.getCommoditiesById);
-    // this.router.post(`${this.path}`,  this.commoditysController.createCommodity);
-    // this.router.put(`${this.path}/:id(\\d+)`,  validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    // this.router.delete(`${this.path}/:id(\\d+)`,  this.usersController.deleteUser);
+    this.router.post(`${this.path}`,  this.commoditysController.createCommodity);
+    this.router.put(`${this.path}/:id`,  this.commoditysController.updateCommodity);
+    this.router.delete(`${this.path}/:id`,  this.commoditysController.deletecommodity);
     // this.router.post(`${this.path}/changepassword`,  this.usersController.changepassword);
   }
 }
