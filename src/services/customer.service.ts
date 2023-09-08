46330a00f0ca0105f161ad3a6e006a56d5c137de
sujata <sujatade1999@gmail.com>
2023-09-08 18:28:53 +0530

    customer_service

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -14,10 +14,10 @@ class CustomerService {
 
   public async signup(userData: any): Promise<String> {
     if (isEmpty(userData)) throw new HttpException(400, 'Invalid user Data');
-    const findUser: Customer = await this.customer.findOne({ where: { customer_phone: userData.customer_phone } });
-    if (findUser) throw new HttpException(500, `Your email ${userData.email} already exists`);
-    const hashedPassword = await hash(userData.customer_password, 10);
-    const createUserData: Customer = await this.customer.create({ ...userData, customer_password: hashedPassword, reset_flag: 1, status: 1 });
+    const findUser: Customer = await this.customer.findOne({ where: { customer_phone: userData.data.customer_phone } });
+    if (findUser) throw new HttpException(500, `Your phoneno ${userData.data.customer_phone} already exists`);
+    const hashedPassword = await hash(userData.data.customer_password, 10);
+    const createUserData: Customer = await this.customer.create({ ...userData.data, customer_password: hashedPassword, reset_flag: 1, status: 1 });
     return `Your ${createUserData.customer_phone} is successfully Registerd`;
   }
 
